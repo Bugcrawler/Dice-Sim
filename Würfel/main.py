13ba3cdd66a1b30bb3caeb4408d1ed78d1948940
Dominik Broder <dominik.b.broder@gmail.com>
2022-03-16 16:32:47 +0100

    Version 1.01
    Update of dice-sim

--- Würfel/main.py
+++ Würfel/main.py
@@ -1,34 +1,34 @@
+# imported modules
 import random
 import time
+
+# Variables
 Playing = True
 
-# Mache eine schleife
-# Mache eine eingabe
 
-# While-loop
+# Dice-loop
 while Playing:
     while True:
         time.sleep(1)
-        choice = input("Do you want to roll the dice?")
+        Choice = input("Do you want to roll the dice? (Y/N)")
 
-        if choice == "y":
+        # Case 1: Rolling the dice.
+        if Choice == "Y":
             print("let's start")
             random_number = random.randint(1, 6)
             time.sleep(2)
-            print("Rolling Dice...")
+            print("rolling...")
             time.sleep(1)
             print("...rolling...")
             time.sleep(1)
+            print("And the number is...")
+            time.sleep(1)
             print(random_number)
             time.sleep(2)
-        elif choice == "n":
+
+        # Case 2: Quitting the sim.
+        elif Choice == "N":
             print("Good Bye")
             quit()
         else:
-            print("Stop this nonsens")
-
-
-random_number = random.randint(1, 6)
-
-print(random_number)
-
+            print("Stop this nonsense.")
